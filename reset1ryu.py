466c04e441780d0d06005ceaa59ccc41ab7b756d
TheOne-Byte <77155464+TheOne-Byte@users.noreply.github.com>
2026-05-25 00:10:26 +1000

    Update reset1ryu.py

--- reset1ryu.py
+++ reset1ryu.py
@@ -1,60 +1,53 @@
 acls:
-  allow_server:
-    # h1 -> server
-    - rule:
-        dl_type: 0x0800
-        ip_proto: 6
-        ipv4_src: 10.0.0.1
-        ipv4_dst: 10.0.0.11
-        actions:
-          allow: 1
+    allow_server:
+        - rule:
+            dl_type: 0x0800
+            ip_proto: 6
+            ipv4_src: 10.0.0.1
+            ipv4_dst: 10.0.0.11
+            actions:
+                allow: 1
 
-    # server -> h1
-    - rule:
-        dl_type: 0x0800
-        ip_proto: 6
-        ipv4_src: 10.0.0.11
-        ipv4_dst: 10.0.0.1
-        actions:
-          allow: 1
+        - rule:
+            dl_type: 0x0800
+            ip_proto: 6
+            ipv4_src: 10.0.0.11
+            ipv4_dst: 10.0.0.1
+            actions:
+                allow: 1
 
-    # h2 -> server
-    - rule:
-        dl_type: 0x0800
-        ip_proto: 6
-        ipv4_src: 10.0.0.2
-        ipv4_dst: 10.0.0.11
-        actions:
-          allow: 1
+        - rule:
+            dl_type: 0x0800
+            ip_proto: 6
+            ipv4_src: 10.0.0.2
+            ipv4_dst: 10.0.0.11
+            actions:
+                allow: 1
 
-    # server -> h2
-    - rule:
-        dl_type: 0x0800
-        ip_proto: 6
-        ipv4_src: 10.0.0.11
-        ipv4_dst: 10.0.0.2
-        actions:
-          allow: 1
+        - rule:
+            dl_type: 0x0800
+            ip_proto: 6
+            ipv4_src: 10.0.0.11
+            ipv4_dst: 10.0.0.2
+            actions:
+                allow: 1
 
-    # mgmt -> server
-    - rule:
-        dl_type: 0x0800
-        ip_proto: 6
-        ipv4_src: 10.0.0.254
-        ipv4_dst: 10.0.0.11
-        actions:
-          allow: 1
+        - rule:
+            dl_type: 0x0800
+            ip_proto: 6
+            ipv4_src: 10.0.0.254
+            ipv4_dst: 10.0.0.11
+            actions:
+                allow: 1
 
-    # server -> mgmt
-    - rule:
-        dl_type: 0x0800
-        ip_proto: 6
-        ipv4_src: 10.0.0.11
-        ipv4_dst: 10.0.0.254
-        actions:
-          allow: 1
+        - rule:
+            dl_type: 0x0800
+            ip_proto: 6
+            ipv4_src: 10.0.0.11
+            ipv4_dst: 10.0.0.254
+            actions:
+                allow: 1
 
-    # DROP EVERYTHING ELSE
-    - rule:
-        actions:
-          drop: 1
+        - rule:
+            actions:
+                drop: 1
