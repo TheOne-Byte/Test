67448ac2160d026553dac784a469cc20bbae6c9f
TheOne-Byte <77155464+TheOne-Byte@users.noreply.github.com>
2026-05-24 23:40:51 +1000

    Update reset1.py

--- reset1.py
+++ reset1.py
@@ -1,6 +1,6 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import OVSController, OVSKernelSwitch
+from mininet.node import OVSKernelSwitch
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
@@ -9,10 +9,10 @@ class EnterpriseTopo(Topo):
     def build(self):
 
         # Hosts
-        h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
-        h2 = self.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
-        mgmt = self.addHost('mgmt', ip='10.0.0.254/24', mac='00:00:00:00:00:03')
-        server = self.addHost('server', ip='10.0.0.11/24', mac='00:00:00:00:00:04')
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+        mgmt = self.addHost('mgmt', ip='10.0.0.254/24')
+        server = self.addHost('server', ip='10.0.0.11/24')
 
         # Switches
         sw1 = self.addSwitch('sw1')
@@ -27,7 +27,7 @@ class EnterpriseTopo(Topo):
         self.addLink(mgmt, sw5, cls=TCLink, bw=10, delay='2ms')
         self.addLink(server, sw5, cls=TCLink, bw=1000, delay='2ms')
 
-        # Inter-switch links (from assignment diagram)
+        # Inter-switch links
         self.addLink(sw1, sw3, cls=TCLink, bw=100, delay='2ms')
         self.addLink(sw2, sw3, cls=TCLink, bw=100, delay='2ms')
         self.addLink(sw1, sw4, cls=TCLink, bw=100, delay='2ms')
@@ -35,28 +35,20 @@ class EnterpriseTopo(Topo):
         self.addLink(sw3, sw5, cls=TCLink, bw=1000, delay='2ms')
         self.addLink(sw4, sw5, cls=TCLink, bw=1000, delay='2ms')
 
-
 def run():
     topo = EnterpriseTopo()
 
-    # IMPORTANT: Use Mininet's built‑in controller (learning switch)
+    # Faucet is the controller, so controller=None
     net = Mininet(
         topo=topo,
-        controller=OVSController,
+        controller=None,
         switch=OVSKernelSwitch,
-        link=TCLink,
-        autoSetMacs=False,
-        autoStaticArp=False
+        link=TCLink
     )
 
     net.start()
-
-    print("\n*** Testing connectivity (pingall) ***")
-    net.pingAll()
-
     CLI(net)
     net.stop()
-
 
 if __name__ == '__main__':
     setLogLevel('info')
